62353da32d8d6f9d182fd4de8aecd9b482292a3c
Prudhvi Nelaturi <prudhvinelaturi29@gmail.com>
2024-04-14 00:58:56 -0400

    Implemented create and get data functions

--- data/properties.js
+++ data/properties.js
@@ -1,21 +1,49 @@
 // This data file should export all functions using the ES6 standard as shown in the lecture code
+import { properties } from '../config/mongoCollections.js';
+import { ObjectId } from 'mongodb';
 
-const create = async (
+export const create = async (
   address,
-  ownerId,
   price,
+  ownerId,
   location,
-  favouriteCount,
   images,
-  details,
-  comments
+  details
 ) => {
-  
+  let newProperty = {
+    address: address,
+    price: price,
+    ownerId: ownerId,
+    location: location,
+    favouriteCount: 0,
+    images: images,
+    details: details,
+    comments: [],
+  };
+
+  let propertyCollection = await properties();
+  const insertInfo = await propertyCollection.insertOne(newProperty);
+  if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+    throw new Error("Couldn't add property");
+  }
+  const newId = insertInfo.insertedId.toString();
+  const theProperty = get(newId);
+  return theProperty;
 };
 
 const getAll = async () => {};
 
-const get = async (propertyId) => {};
+export const get = async (propertyId) => {
+  // const id = validation.checkId(propertyId);
+  let propertyCollection = await properties();
+  const theProperty = await propertyCollection.findOne({
+    _id: new ObjectId(propertyId),
+  });
+  if (!theProperty) {
+    throw new Error('No property with that id');
+  }
+  return theProperty;
+};
 
 const remove = async (propertyId) => {};
 
