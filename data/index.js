5c8ce0e2883953afc7dbfd62580911bf5dbc130f
varunnihal <varunnihal18@gmail.com>
2024-04-21 15:50:18 -0400

    Updated index of data functions

--- data/index.js
+++ data/index.js
@@ -1,1 +1,6 @@
 // This file should import both data files and export them as shown in the lecture code
+import * as users from './user.js';
+import * as properties from './properties.js';
+import * as comments from './comments.js';
+
+export {users, properties, comments};
