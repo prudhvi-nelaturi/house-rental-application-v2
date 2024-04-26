18ceb2d78b557947b7026c7b99bac5539ad70423
Prudhvi Nelaturi <prudhvinelaturi29@gmail.com>
2024-04-26 18:51:56 -0400

    Updated property handlebar

--- routes/properties.js
+++ routes/properties.js
@@ -81,39 +81,49 @@ router
   .route('/:propertyId')
   .get(async (req, res) => {
     //code here for GET
-  try {
-    if (!req.params.propertyId) throw `Error: You must supply a valid propertyId!`;
-    if (typeof req.params.propertyId !== 'string') throw `Error: value must be a string!`;
-    req.params.propertyId = req.params.propertyId.trim();
-    if (req.params.propertyId.length === 0)
-      throw `Error: value cannot be an empty string or string with just spaces`;
-  } catch (error) {
-    res.status(400).render("searchResults", {error: error, hasError: true, title: "Search Results"});
-  }
-  try {
-    const propertyDetails = await properties.get(req.params.propertyId);
-    res.render('property', {title: "Property", propertyDetails: propertyDetails});
-  } catch (e) {
-    res.status(404).render('error', { title: "error", error: e});
-  }
+    try {
+      if (!req.params.propertyId)
+        throw `Error: You must supply a valid propertyId!`;
+      if (typeof req.params.propertyId !== 'string')
+        throw `Error: value must be a string!`;
+      req.params.propertyId = req.params.propertyId.trim();
+      if (req.params.propertyId.length === 0)
+        throw `Error: value cannot be an empty string or string with just spaces`;
+    } catch (error) {
+      res
+        .status(400)
+        .render('searchResults', {
+          error: error,
+          hasError: true,
+          title: 'Search Results',
+        });
+    }
+    try {
+      const propertyDetails = await properties.get(req.params.propertyId);
+      res.render('property', {
+        title: 'Property',
+        propertyDetails: propertyDetails,
+      });
+    } catch (e) {
+      res.status(404).render('error', { title: 'error', error: e });
+    }
   })
   .delete(async (req, res) => {
     //code here for DELETE
     try {
       req.params.propertyId = validateId(req.params.propertyId, 'Id URL Param');
     } catch (e) {
-      return res.status(400).render('userPage', {title: "userPage"})
+      return res.status(400).render('userPage', { title: 'userPage' });
     }
     try {
       let deletedProperty = await properties.remove(req.params.propertyId);
-      if(deletedProperty.deleted){
-        res.status(200).json({message: "Property deleted successfully"});
-      }
-      else{
-        res.status(404).json({message: "Unable to delete the property"});
+      if (deletedProperty.deleted) {
+        res.status(200).json({ message: 'Property deleted successfully' });
+      } else {
+        res.status(404).json({ message: 'Unable to delete the property' });
       }
     } catch (e) {
-      res.status(404).json({error: e});
+      res.status(404).json({ error: e });
     }
   })
   .put(async (req, res) => {
