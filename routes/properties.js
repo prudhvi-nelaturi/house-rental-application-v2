4ddbc3b5ff0a331c23deea7dac3f68ca6dcf3db2
Sakshi Sherkar <ssherkar1@stevens.edu>
2024-05-02 16:22:11 -0400

    Add property handlebar, routes changes

--- routes/properties.js
+++ routes/properties.js
@@ -52,23 +52,50 @@ router
       if (searchResults) {
         return res.render('searchResults', {
           title: 'searchResults',
+          searchResults: searchResults,
         });
       }
     } catch (error) {
       return res.status(404).render('error', { title: 'Error', error: error });
     }
   })
+
+  router
+  .route('/')
   .post(async (req, res) => {
     //code here for POST
     let propertyInfo = req.body;
+    let address = {
+      street : propertyInfo.street,
+      apartmentNum : propertyInfo.apartmentNum,
+      city : propertyInfo.city,
+      state : propertyInfo.state,
+      zip : propertyInfo.zip
+    }
+    let details = {
+      description: propertyInfo.description,
+      propertyType: propertyInfo.propertyType,
+      apartmentType: propertyInfo.apartmentType,
+      accomodationType: propertyInfo.accomodationType,
+      area: propertyInfo.area,
+      bedroomCount: propertyInfo.bedroomCount,
+      bathroomCount: propertyInfo.bathroomCount
+    }
+    let location = {
+      latitude: propertyInfo.latitude,
+      longitude: propertyInfo.longitude
+    }
+    let ownerFullName = req.session.user.firstName + '  ' + req.session.user.lastName;
     try {
       const newProduct = await properties.create(
-        propertyInfo.address,
+        address,
         propertyInfo.price,
-        propertyInfo.ownerId,
-        propertyInfo.location,
+        req.session.user.id,
+        ownerFullName,
+        location,
         propertyInfo.images,
-        propertyInfo.details
+        details,
+        propertyInfo.nearestLandmarks
       );
       res.render('home');
       // return res.json(newUser);
@@ -77,11 +104,54 @@ router
     }
   });
 
+router.route('/property/:propertyId').get(async (req, res) => {
+  //code here for GET
+  try {
+    if (!req.params.propertyId)
+      throw `Error: You must supply a valid propertyId!`;
+    if (typeof req.params.propertyId !== 'string')
+      throw `Error: value must be a string!`;
+    req.params.propertyId = req.params.propertyId.trim();
+    if (req.params.propertyId.length === 0)
+      throw `Error: value cannot be an empty string or string with just spaces`;
+  } catch (error) {
+    res.status(400).render('searchResults', {
+      error: error,
+      hasError: true,
+      title: 'Search Results',
+    });
+  }
+  try {
+    const propertyDetails = await properties.get(req.params.propertyId);
+    res.render('property', {
+      title: 'Property',
+      propertyDetails: propertyDetails,
+    });
+  } catch (e) {
+    res.status(404).render('error', { title: 'error', error: e });
+  }
+});
+
 router
-  .route('/:propertyId')
-  .get(async (req, res) => {})
+  .route('/remove/:propertyId')
   .delete(async (req, res) => {
     //code here for DELETE
+    try {
+      req.params.propertyId = validateId(req.params.propertyId, 'Id URL Param');
+    } catch (e) {
+      return res.status(400).render('userPage', { title: 'userPage' });
+    }
+    try {
+      let deletedProperty = await properties.remove(req.params.propertyId,req.session.user.id);
+      if (deletedProperty.deleted) {
+        //res.status(200).json({message: "Property deleted successfully"});
+        return res.status(200).redirect('/userProfile');
+      } else {
+        res.status(404).json({ message: 'Unable to delete the property' });
+      }
+    } catch (e) {
+      res.status(404).json({ error: e });
+    }
   })
   .put(async (req, res) => {
     //code here for PUT
