4611f045433c65c0fa6331c54c756da297593f04
Pavankalyan81 <lingineni752@gmail.com>
2024-05-04 16:18:45 -0400

    frontend of searchResults

---
+++ public/js/maps.js
@@ -0,0 +1,17 @@
+let script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBkxUnfDD2_0dFdS0AQWyNMwDEHbE-2aME&callback=initMap';
+script.defer = true;
+script.async = true;
+
+const latitude = parseFloat(document.currentScript.getAttribute('latitude'));
+const longitude = parseFloat(document.currentScript.getAttribute('longitude'));
+
+window.initMap = function() {
+    let gmap = new google.maps.Map(document.getElementById('gmap'), {
+        center: {lat: latitude, lng: longitude},
+        zoom: 16
+    });
+    let marker = new google.maps.Marker({position: {lat: latitude, lng: longitude}, map: gmap});
+}
+
+document.head.appendChild(script);
--- public/js/propertyResults_favorite.js
+++ public/js/propertyResults_favorite.js
@@ -1,10 +1,19 @@
-$('.favorite-icon').click(function() {
-    $(this).toggleClass('active');
-
-    const isFavorite = $(this).hasClass('active');
-    const propertyIndex = $(this).closest('.card').index();
-    if(isFavorite) searchResults[propertyIndex].favoriteCount++;
-    else searchResults[propertyIndex].favoriteCount--;
-
-    $(this).closest('.card').find('.property-details .favorite-count').text(searchResults[propertyIndex].favoriteCount);
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const favoriteButtons = document.querySelectorAll('.add-favorites-btn');
+  
+    favoriteButtons.forEach(button => {
+      button.addEventListener('click', function(event) {
+        const propertyId = event.target.getAttribute('data-property-id');
+        const property = searchResults.find(property => property._id === propertyId);
+  
+        if (property.favouriteCount) {
+          property.favouriteCount--;
+        } else {
+          property.favouriteCount = 1;
+        }
+  
+        // Update the UI
+        event.target.textContent = property.favouriteCount > 0? `Remove from Favorites (${property.favouriteCount})` : 'Add to Favorites';
+      });
+    });
+  });
