05b56083b06c1793d68760a9f8e8e5c626d7c22a
Prudhvi Nelaturi <prudhvinelaturi29@gmail.com>
2024-05-06 14:47:17 -0400

    Updated some code

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,47 +1,45 @@
-$(document).ready(()=>{
-    let ads = $('#ads');
-    let reqConfig = {
-        method: 'GET',
-        url: '/adsRandom'
+$(document).ready(() => {
+  let ads = $('#ads');
+  let reqConfig = {
+    method: 'GET',
+    url: '/adsRandom',
+  };
+
+  $.ajax(reqConfig).then((resMsg) => {
+    if (resMsg && resMsg.propLen == 0) {
+      ads.html('');
+    } else {
+      showAd(resMsg, ads);
     }
+  });
 
-    $.ajax(reqConfig).then((resMsg)=>{
-        if(resMsg && resMsg.propLen == 0) {
-            ads.html("");
-        }
-        else {
-            showAd(resMsg, ads);
-        }
-    })
+  //2nd ad
+  let ads2 = $('#ads2');
+  let reqConfig2 = {
+    method: 'GET',
+    url: '/adsLastProp',
+  };
 
-    //2nd ad
-    let ads2 = $('#ads2');
-    let reqConfig2 = {
-        method: 'GET',
-        url: '/adsLastProp'
+  $.ajax(reqConfig2).then((resMsg2) => {
+    if (resMsg2 && resMsg2.propLen == 0) {
+      ads2.html('');
+    } else {
+      showAd(resMsg2, ads2);
     }
-
-    $.ajax(reqConfig2).then((resMsg2)=>{
-        if(resMsg2 && resMsg2.propLen == 0) {
-            ads2.html("");
-        }
-        else {
-            showAd(resMsg2, ads2);
-        }
-    })
+  });
 });
 
 function showAd(resMsg, propAd) {
-    let x = resMsg;
-            let content = $(
-                `<p><strong>Sponsored Ads: </strong></p>
+  let x = resMsg;
+  let content = $(
+    `<p><strong>Sponsored Ads: </strong></p>
                 <img src="/uploads/images/${x.images[0]}" alt="Property Image" class="ads-image" onerror="this.onerror=null; this.src='/uploads/images/propImage.jpeg'">
                 <p><strong>House: </strong>${x.address.street}, ${x.address.city}, ${x.address.state}</p>
                 <p><strong>Posted By: </strong>${x.ownerFullName}</p>
                 <p><strong>Rent: </strong>${x.price}$</p>
                 <p><strong>Type: </strong>${x.details.apartmentType}, ${x.details.propertyType}</p>
                 <a href="/search/property/${x._id}">Go To Property</a>`
-            );
-            propAd.append(content);
-            propAd.show();
+  );
+  propAd.append(content);
+  propAd.show();
 }
